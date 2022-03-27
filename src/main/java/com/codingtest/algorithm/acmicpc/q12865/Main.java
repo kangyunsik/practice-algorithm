5f23543ff3f60cfbd57f77a417d4a847f6463fe6
kangyunsik <re12io@ajou.ac.kr>
2022-03-27 16:32:06 +0900

    fix: acmicpc 12865. 입력값을 받는 동시에 DP 를 수행하도록 개선.

--- src/main/java/com/codingtest/algorithm/acmicpc/q12865/Main.java
+++ src/main/java/com/codingtest/algorithm/acmicpc/q12865/Main.java
@@ -14,29 +14,21 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        int[][] items = new int[n][2];
         int[] dp = new int[k + 1];
+        int weight;
+        int value;
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            items[i][0] = Integer.parseInt(st.nextToken());
-            items[i][1] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = k - items[i][0]; j >= 0; j--) {
-                if (dp[j] >= 0) {
-                    dp[j + items[i][0]] = Math.max(dp[j + items[i][0]], dp[j] + items[i][1]);
-                }
+            weight = Integer.parseInt(st.nextToken());
+            value = Integer.parseInt(st.nextToken());
+            for (int j = k - weight; j >= 0; j--) {
+                dp[j + weight] = Math.max(dp[j + weight], dp[j] + value);
             }
         }
 
         int ans = 0;
-        for (int i : dp) {
-            ans = Math.max(ans, i);
-        }
+        for (int i : dp) ans = Math.max(ans, i);
         System.out.println(ans);
     }
 }
