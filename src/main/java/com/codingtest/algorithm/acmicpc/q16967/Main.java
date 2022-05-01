8165188ca807319d269180adbd9cf96f2f5bbad2
kangyunsik <re12io@ajou.ac.kr>
2022-05-02 01:15:11 +0900

    acmicpc 16967 해결. `구현` 문제. brr(a,b) = arr(a,b) + arr(a+x, b+y) 이므로, 이항시켜, arr 값을 저장하고 출력하도록 구현.

---
+++ src/main/java/com/codingtest/algorithm/acmicpc/q16967/Main.java
@@ -0,0 +1,37 @@
+package com.codingtest.algorithm.acmicpc.q16967;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        arr = new int[h][w];
+        for (int i = 0; i < h; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < w; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken()) - getArr(i - x, j - y);
+            }
+        }
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                sb.append(anInt).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static int getArr(int i, int j){
+        if(i < 0 || j < 0) return 0;
+        return arr[i][j];
+    }
+}
